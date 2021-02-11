b0e9c25114e4390fb697c9d01ca6944cd1490652
kishore.sk <kishore.sk@zohocorp.com>
2021-02-11 15:31:43 +0530

    Sample Project push

---
+++ sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by Kishore S on 11/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      print("hello every one")
+    }
+
+
+}
+
