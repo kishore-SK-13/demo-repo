0b0807a6532827cca319348d58317dbec1dfdec4
kishore.sk <kishore.sk@zohocorp.com>
2021-02-11 18:40:51 +0530

    File added

---
+++ sample/Sample.swift
@@ -0,0 +1,16 @@
+//
+//  Sample.swift
+//  sample
+//
+//  Created by Kishore S on 11/02/21.
+//
+
+import Foundation
+
+class Sample {
+    
+    func  sub()  {
+        print("This is to subtract")
+    }
+    
+}
